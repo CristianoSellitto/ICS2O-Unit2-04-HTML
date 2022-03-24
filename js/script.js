70e553a610e8e0a15eeef74d4b072229092c30d3
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-24 16:37:00 +0000

    Changed target shape

--- js/script.js
+++ js/script.js
@@ -4,11 +4,10 @@
 
 'use strict'
 /**
- * Calculates area and perimeter of the user's rectangle.
+ * Calculates area and perimeter of the user's triangle.
  */
 function calculate() {
   const length = parseInt(document.getElementById("lengthinput").value)
   const width = parseInt(document.getElementById("widthinput").value)
-  document.getElementById("perimeteroutput").innerHTML = "<p>The perimeter of this rectangle is " + ((length * 2) + (width * 2)) + "cm</p>"
-  document.getElementById("areaoutput").innerHTML = "<p>The area of this rectangle is " + (length * width) + "cm²</p>"
+  document.getElementById("areaoutput").innerHTML = "<p>The area of this triangle is " + (length * width / 2) + "cm²</p>"
 }
